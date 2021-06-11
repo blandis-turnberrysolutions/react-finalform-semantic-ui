870a6b51cae6e929e5a60238c21b12a28b9180e3
Bradley T. Landis <blandis@turnberrysolutions.com>
2021-06-11 11:41:42 -0400

    update to use yup for validation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import "./App.css";
 import { Form as FinalForm, Field as FinalFormField } from "react-final-form";
+import { setIn } from "final-form";
 import {
   Button,
   Form as SemanticForm,
   Header,
   Container,
-  Dropdown,
 } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
+import * as yup from "yup";
+
+const validationSchema = yup.object({
+  firstName: yup.string().required("Required"),
+  lastName: yup.string().required("Required"),
+  interests: yup
+    .array()
+    .of(yup.string())
+    .min(1, "Must select at least 1 interest"),
+});
+
+const validateFormValues = (schema) => async (values) => {
+  const schemaToValidate = typeof schema === "function" ? schema() : schema;
+
+  try {
+    await schemaToValidate.validate(values, { abortEarly: false });
+  } catch (err) {
+    const errors = err.inner.reduce(
+      (formError, innerError) =>
+        setIn(formError, innerError.path, innerError.message),
+      {}
+    );
+
+    return errors;
+  }
+};
+
+const validate = validateFormValues(validationSchema);
 
 function App() {
   const onSubmit = (values) => {
     alert(JSON.stringify(values));
   };
-  const required = (value) => (value ? undefined : "Required");
-  const notEmpty = (value) => (value?.length > 0 ? undefined : "Required");
 
   return (
     <div className="App">
       <FinalForm
         onSubmit={onSubmit}
+        validate={validate}
         initialValues={{ firstName: "", lastName: "", interests: [] }}
         render={({ handleSubmit }) => (
           <div>
@@ -29,7 +56,7 @@ function App() {
             <Container>
               <SemanticForm onSubmit={handleSubmit}>
                 <SemanticForm.Group widths="equal">
-                  <FinalFormField name="firstName" validate={required}>
+                  <FinalFormField name="firstName">
                     {({ input, meta }) => (
                       <SemanticForm.Input
                         fluid
@@ -43,7 +70,7 @@ function App() {
                       />
                     )}
                   </FinalFormField>
-                  <FinalFormField name="lastName" validate={required}>
+                  <FinalFormField name="lastName">
                     {({ input, meta }) => (
                       <SemanticForm.Input
                         fluid
@@ -58,30 +85,30 @@ function App() {
                   </FinalFormField>
                 </SemanticForm.Group>
                 <SemanticForm.Group widths="equal">
-                  <FinalFormField name="interests" validate={notEmpty}>
+                  <FinalFormField name="interests">
                     {({ input, meta }) => (
-                      <SemanticForm.Field error={meta.touched && meta.error}>
-                        <label htmlFor="interests">Interests</label>
-                        <Dropdown
-                          multiple
-                          search
-                          selection
-                          placeholder="Select Interests"
-                          fluid
-                          options={[
-                            {
-                              key: "Running",
-                              text: "Running",
-                              value: "Running",
-                            },
-                            { key: "Hiking", text: "Hiking", value: "Hiking" },
-                            { key: "Biking", text: "Biking", value: "Biking" },
-                          ]}
-                          name={input.name}
-                          value={input.value}
-                          onChange={(e, v) => input.onChange(v.value)}
-                        />
-                      </SemanticForm.Field>
+                      <SemanticForm.Field
+                        error={meta.touched && meta.error}
+                        label="Interests"
+                        control={SemanticForm.Select}
+                        multiple
+                        search
+                        selection
+                        placeholder="Select Interests"
+                        fluid
+                        options={[
+                          {
+                            key: "Running",
+                            text: "Running",
+                            value: "Running",
+                          },
+                          { key: "Hiking", text: "Hiking", value: "Hiking" },
+                          { key: "Biking", text: "Biking", value: "Biking" },
+                        ]}
+                        name={input.name}
+                        value={input.value}
+                        onChange={(e, v) => input.onChange(v.value)}
+                      />
                     )}
                   </FinalFormField>
                 </SemanticForm.Group>
